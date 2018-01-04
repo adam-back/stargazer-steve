31a5ca844e2eb9e63121cc06133ff7d5f1c4b540
Adam Jaffe Back <adb33@email.arizona.edu>
2018-01-03 21:45:22 -0800

    (init) Basic Hello World server.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
