c6a3098298be39437b7d7526a15136d41d60b8bb
Adam Jaffe Back <adb33@email.arizona.edu>
2018-01-05 21:50:26 -0800

    (feat) Hook successfully sends generic response using v2 API.

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/', (req, res) => {
+  var response = {
+    fulfillmentText: 'Greetings'
+  }
+  // console.log('received hook 0.0.1', req.body)
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify(response));
 });
 
 const server = app.listen(8080, () => {
